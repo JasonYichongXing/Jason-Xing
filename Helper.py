349678acf3f7ed4a73e13b4dc8e934fa42777e62
Jason Xing <37820014+JasonYichongXing@users.noreply.github.com>
2018-06-18 15:47:11 -0400

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -2,10 +2,12 @@ import calendar
 import numpy as np
 import pandas as pd
 
+###############################
+# Last Business Day
+###############################
 
 def lastbusinessday(year, month):
     """ return the last business day of the month """
-    
     _, day = calendar.monthrange(year, month)
     weekday = calendar.weekday(year, month, day)
     if weekday == 6:
@@ -14,15 +16,14 @@ def lastbusinessday(year, month):
         return day - 1
     return day
 
-
 # or using pandas function:
-import pandas as pd
-
 pd.date_range('2018', periods= 12, freq='CBM') # Last Business Day of every month
 pd.date_range('2018', periods= 12, freq='BMS') # First Business Day of every month
 
 
-
+###############################
+# Return
+###############################
 # To calculate the simple returns and log returns of a DataFrame
 # Here, we skip the NA check to speed up the process
 # The Pandas BIF pct_change() has the fillna and freq options, which will be very slow if our data dimension is a bit large.
@@ -47,5 +48,19 @@ def pct(data):
         raise ValueError('ERROR: one or more elements of the first row are zero.')
     return data/data.iloc[0] - 1
         
+        
+###############################
+# list file names and last modified time
+############################### 
+from os import listdir
+from os.path import isfile, join, getmtime
+from datetime import datetime
+
+def listfile(path):
+    for f in listdir(path):
+        if isfile(join(path,f)):
+            ftime = getmtime(join(path, f))
+            ftime = datetime.fromtimestamp(ftime).strftime('%y-%m-%d %H:%M:%S')
+        yield f, ftime
         
         
